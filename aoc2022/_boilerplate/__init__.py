a00a34be981a4655835f39c5181bd7d2f91b66b1
Matthew Wyant <me@zanfar.com>
2022-12-03 07:03:58 -0700

    Template updates

--- aoc2022/_boilerplate/__init__.py
+++ aoc2022/_boilerplate/__init__.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 from typing import Optional, Any
 
+PUZZLE_INPUT_FILENAME = Path("aoc2022/day{{day_full}}/puzzle_input.txt")
+
 
 def parse_data(input_filename: str | Path):
     """Read a datafile and return the representative dataset"""
@@ -14,15 +16,15 @@ def parse_data(input_filename: str | Path):
     return ""
 
 
-def solve_part_a():
+def solve_part_a(filename: str = PUZZLE_INPUT_FILENAME):
     """Part A Solution"""
 
-    dataset = parse_data("aoc2022/day{{day_full}}/puzzle_input.txt")
+    dataset = parse_data(filename)
     return len(dataset)
 
 
-def solve_part_b():
+def solve_part_b(filename: str = PUZZLE_INPUT_FILENAME):
     """Part B Solution"""
 
-    dataset = parse_data("aoc2022/day{{day_full}}/puzzle_input.txt")
+    dataset = parse_data(filename)
     return len(dataset)
--- aoc2022/_boilerplate/test_day.py
+++ aoc2022/_boilerplate/test_day.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-from aoc2022.day{{day_full}} import parse_data
+from aoc2022.day{{day_full}} import parse_data, solve_part_a, solve_part_b
 
 TEST_INPUT_FILENAME = Path("aoc2022/day{{day_full}}/test_input.txt")
 
@@ -10,3 +10,11 @@ TEST_INPUT_FILENAME = Path("aoc2022/day{{day_full}}/test_input.txt")
 def test_import_and_parse():
     dataset = parse_data(TEST_INPUT_FILENAME)
     assert dataset
+
+
+def test_solution_a():
+    assert solve_part_a(TEST_INPUT_FILENAME) == 0
+
+
+def test_solution_b():
+    assert solve_part_b(TEST_INPUT_FILENAME) == 0
